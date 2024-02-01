807119bdec2ed290254ea9ed5e4d3f4d267ecf17
Aires Gonçalves <airesvsg@gmail.com>
2024-02-01 14:55:11 -0300

    adding openApiBaseChain

---
+++ src/services/chain/openapi-base-chain.ts
@@ -0,0 +1,59 @@
+import { CallbackManagerForChainRun } from "langchain/callbacks";
+import { BaseChain, createOpenAPIChain } from "langchain/chains";
+import { BaseChatModel } from "langchain/chat_models/base";
+import { BaseFunctionCallOptions } from "langchain/dist/base_language";
+import { ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate } from "langchain/prompts";
+import { ChainValues } from "langchain/schema";
+import type { OpenAPIV3_1 } from "openapi-types";
+
+export type OpenApiBaseChainInput = {
+    spec: string | OpenAPIV3_1.Document<{}>;
+    llm?: BaseChatModel<BaseFunctionCallOptions>;
+    customMessage?: string;
+    headers: Record<string, string>;
+};
+
+export class OpenApiBaseChain extends BaseChain {
+    readonly inputKey = "query";
+    readonly outputKey = "result";
+    private input: OpenApiBaseChainInput;
+
+    constructor(input: OpenApiBaseChainInput) {
+        super();
+        this.input = input;
+    }
+
+    get inputKeys(): string[] {
+        return [this.inputKey];
+    }
+
+    get outputKeys(): string[] {
+        return [this.outputKey];
+    }
+
+    private getOpenApiPrompt(): ChatPromptTemplate {
+        const prompt = `You are an AI with expertise in OpenApi and Swagger.
+Always answer the question in the language in which the question was asked.
+- Always respond with the URL;
+- Never put information or explanations in the answer;
+${this.input.customMessage || ''}`;
+        return ChatPromptTemplate.fromMessages([
+            SystemMessagePromptTemplate.fromTemplate(prompt),
+            HumanMessagePromptTemplate.fromTemplate('{question}'),
+        ]);
+    }
+
+    async _call(values: ChainValues, runManager?: CallbackManagerForChainRun): Promise<ChainValues> {
+        const question = values[this.inputKey];
+        const chain = await createOpenAPIChain(this.input.spec, { 
+            llm: this.input.llm,
+            prompt: this.getOpenApiPrompt(),
+            headers: this.input.headers
+        });
+        return await chain.invoke({ question });
+    }
+
+    _chainType(): string {
+        return "open_api_chain" as const;
+    }
+}
--- src/services/chain/openapi-chain.ts
+++ src/services/chain/openapi-chain.ts
@@ -1,17 +1,19 @@
-import { APIChain, BaseChain } from 'langchain/chains';
+import { BaseChain } from 'langchain/chains';
 import { IOpenAPIConfig } from '../../interface/agent.interface';
 
 import { BaseChatModel } from 'langchain/chat_models/base';
+import { OpenApiBaseChain } from './openapi-base-chain';
 
 class OpenAPIChain {
   private _settings: IOpenAPIConfig;
 
   constructor(settings: IOpenAPIConfig) {
     this._settings = settings;
+    console.log(this._settings);
   }
 
-  private getHeaders(): any {
-    if (!this._settings.xApiKey && !this._settings.authorization) 
+  private getHeaders(): Record<string, string> | undefined {
+    if (!this._settings.xApiKey && !this._settings.authorization)
       return undefined;
 
     const temp: any = {};
@@ -20,24 +22,18 @@ class OpenAPIChain {
       temp['x-api-key'] = this._settings.xApiKey;
 
     if (!this._settings?.authorization)
-      temp['Authorization'] = this._settings.authorization;  
+      temp['Authorization'] = this._settings.authorization;
 
-    return {
-      ...temp,
-    }
+    return { ...temp };
   }
 
   public async create(llm: BaseChatModel, ...args: any): Promise<BaseChain> {
-    const chainOpenAPI = APIChain.fromLLMAndAPIDocs(
+    return new OpenApiBaseChain({
       llm,
-      this._settings.data,
-      {
-        outputKey: 'openAPIResult',
-        headers: this.getHeaders(),
-      },
-    );
-
-    return chainOpenAPI;
+      spec: this._settings.data,
+      customMessage: this._settings.customizeSystemMessage || '',
+      headers: this.getHeaders(),
+    });
   }
 }
 
