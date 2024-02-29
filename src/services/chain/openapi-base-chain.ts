9660b3e808e3be4687c580b2b452ba4a5e9df455
TUCEGA <TulioCesar.Gaio@jsafrasarasin.com>
2024-02-29 18:50:41 -0300

    feat: add new open api chain

--- src/services/chain/openapi-base-chain.ts
+++ src/services/chain/openapi-base-chain.ts
@@ -1,5 +1,5 @@
 import { CallbackManagerForChainRun } from 'langchain/callbacks';
-import { BaseChain, ChainInputs, createOpenAPIChain } from 'langchain/chains';
+import { BaseChain, ChainInputs } from 'langchain/chains';
 import { BaseChatModel } from 'langchain/chat_models/base';
 import { BaseFunctionCallOptions } from 'langchain/dist/base_language';
 import {
@@ -10,6 +10,7 @@ import {
   SystemMessagePromptTemplate,
 } from 'langchain/prompts';
 import { ChainValues } from 'langchain/schema';
+import { RunnableSequence } from 'langchain/schema/runnable';
 import type { OpenAPIV3_1 } from 'openapi-types';
 
 export interface OpenApiBaseChainInput extends ChainInputs {
@@ -17,6 +18,18 @@ export interface OpenApiBaseChainInput extends ChainInputs {
   llm?: BaseChatModel<BaseFunctionCallOptions>;
   customizeSystemMessage?: string;
   headers: Record<string, string>;
+  timeout?: number;
+}
+
+interface IRequest {
+  url: string;
+  contentType?: string;
+  requestMethod?: string;
+  data: object;
+}
+
+interface IResponseHeaders {
+  [key: string]: string;
 }
 
 export class OpenApiBaseChain extends BaseChain {
@@ -45,7 +58,8 @@ export class OpenApiBaseChain extends BaseChain {
       You should follow the following rules when generating and answer:\n
       - Only execute the request on the service if the question is not in CHAT HISTORY, if the question has already been answered, use the same answer and do not make a request on the service.
       - Only attempt to answer if a question was posed.\n
-      - Always answer the question in the language in which the question was asked.\n\n
+      - Always answer the question in the language in which the question was asked.\n
+      - The response must be an json object contains an url, contentType, requestMethod and data.\n\n
       -------------------------------------------\n
       USER PROMPT: {user_prompt}\n
       -------------------------------------------\n
@@ -72,19 +86,67 @@ export class OpenApiBaseChain extends BaseChain {
     return CHAT_COMBINE_PROMPT;
   }
 
-  private tryParseText(text: string): string {
-    if (text.includes('No function_call in message')) {
-      try {
-        const txtSplitJson = text.split('No function_call in message ')[1];
-        const txtJson = JSON.parse(txtSplitJson);
-        
-        return txtJson[0]?.text;
-      }	catch (error) {
-        return `Sorry, I could not find the answer to your question.`;
-      }	
+  configureTimeout(timeout: number) {
+    let timeoutId = null;
+    const controller = new AbortController();
+    const { signal } = controller;
+
+    if (timeout) {
+      timeoutId = setTimeout(() => {
+        controller.abort();
+      }, timeout);
     }
 
-    return text;
+    return { signal, timeoutId };
+  }
+
+  tryParseJSON = (value: any) => {
+    try {
+      return JSON.parse(value);
+    } catch (error) {
+      return value;
+    }
+  };
+
+  async formatResponse(response: Response) {
+    const body = this.tryParseJSON(await response.text());
+
+    const responseHeaders: IResponseHeaders = {};
+
+    Array.from(response.headers.keys()).forEach((key) => {
+      responseHeaders[key] = response.headers.get(key);
+    });
+
+    const formattedResponse = {
+      body,
+      response: {
+        body,
+        headers: responseHeaders,
+        ok: response.ok,
+        statusCode: response.status,
+        statusText: response.statusText,
+      },
+    };
+
+    return response.ok ? formattedResponse : { body: 'Request Error' };
+  }
+
+  async fetchOpenAPI(data: IRequest, timeout: number) {
+    const abortSignal = this.configureTimeout(timeout);
+
+    const response = await fetch(data?.url, {
+      method: data?.requestMethod,
+      headers: {
+        'Content-Type': data?.contentType,
+        ...this._input.headers,
+      },
+      body: JSON.stringify(data?.data),
+      signal: abortSignal.signal,
+    });
+
+    clearTimeout(abortSignal.timeoutId);
+
+    return this.formatResponse(response);
   }
 
   async _call(
@@ -97,34 +159,48 @@ export class OpenApiBaseChain extends BaseChain {
     const question = values[this.inputKey];
     const schema = this._input.spec;
 
-    const chain = await createOpenAPIChain(this._input.spec, {
-      llm: this._input.llm,
-      prompt: this.buildPromptTemplate(this.getOpenApiPrompt()),
-      headers: this._input.headers,
-      verbose: true,
-    });
+    const fetchSentence = RunnableSequence.from([
+      {
+        schema: () => schema,
+        question: (input: { question: string }) => input.question,
+        chat_history: () => values?.chat_history,
+        format_chat_messages: () => values?.format_chat_messages,
+        user_prompt: () => this._input.customizeSystemMessage || '',
+      },
+      this.buildPromptTemplate(this.getOpenApiPrompt()),
+      this._input?.llm.bind({}),
+    ]);
 
-    let answer:string = '';
+    const finalChain = RunnableSequence.from([
+      {
+        question: (input) => input.question,
+        query: fetchSentence,
+      },
+      {
+        table_info: () => schema,
+        input: () => question,
+        schema: () => schema,
+        question: (input) => input.question,
+        query: (input) => input.query,
+        response: async (input) => {
+          const request: IRequest = JSON.parse(
+            input.query.content.replace('```json', '').replace('```', '')
+          );
+          return await this.fetchOpenAPI(
+            request,
+            this._input.timeout || 120000
+          );
+        },
+      },
+      {
+        [this.outputKey]: (previousStepResult) => {
+          return previousStepResult.response.body;
+        },
+      },
+    ]);
 
-    try {
-      const rs = await chain.invoke({
-        question,
-        schema,
-        chat_history: values?.chat_history,
-        format_chat_messages: values?.format_chat_messages,
-        user_prompt: this._input.customizeSystemMessage || '',
-      });
-  
-      this._logger.log('OPENAPI Resposta: ', answer);
-  
-      answer = rs?.response;
-    } catch (error) {
-      this._logger.error('OPENAPI Error: ', error);
-
-      answer = this.tryParseText(error?.message);
-    } finally { 
-      return { [this.outputKey]: answer };
-    }
+    const result = await finalChain.invoke({ question });
+    return result;
   }
 
   _chainType(): string {
